a3ac9501fa7e5c6910b185e861cf128466a0320f
Mykhailo Kovalenko <mykhaylo2007@gmail.com>
2026-03-28 17:24:19 +0200

    Stage 1 done

---
+++ Week8/Point.cs
@@ -0,0 +1,10 @@
+struct Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public void Print()
+    {
+        Console.WriteLine($"({X}, {Y})");
+    }
+}
---
+++ Week8/PointRef.cs
@@ -0,0 +1,10 @@
+class PointRef
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public void Print()
+    {
+        Console.WriteLine($"({X}, {Y})");
+    }
+}
